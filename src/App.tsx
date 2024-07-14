aec0efa1ae8872751db9435cc0ef9c85d4acf5f0
flAks <83161350+SnrFlaks@users.noreply.github.com>
2024-07-14 13:16:03 +0300

    set up basic site layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app-container">
+      <div className="header-container">
+        <div className="titleContainer">
+          <img src="/logo.png" alt="logo" className="logo" />
+          <h1 className="title">Melodyssey</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
