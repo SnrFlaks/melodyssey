011c674c826fa128d6a030cf717b50004a6ac945
flAks <83161350+SnrFlaks@users.noreply.github.com>
2024-07-14 14:01:46 +0300

    added Info button using MUI for About Us page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { Icon, IconButton } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
       <div className="header-container">
-        <div className="titleContainer">
+        <div className="title-container">
           <img src="/logo.png" alt="logo" className="logo" />
           <h1 className="title">Melodyssey</h1>
         </div>
+        <IconButton className="info-btn" aria-label="Open about us">
+          <Icon className="info-icon">info</Icon>
+        </IconButton>
       </div>
     </div>
   );
