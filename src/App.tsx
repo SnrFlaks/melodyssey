d459320c6c69491c485310fb3b67663b8ed542dd
flAks <83161350+SnrFlaks@users.noreply.github.com>
2024-07-16 23:42:47 +0300

    made some minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Icon, IconButton } from "@mui/material";
 import Player from "./Player";
 import "./App.css";
 
@@ -9,6 +12,7 @@ function App() {
   const [spotifyUrl, setSpotifyUrl] = useState<string>(defaultPlaylistUrl);
   const [type, setType] = useState<string>("");
   const [id, setId] = useState<string>("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     extractTypeAndId(defaultPlaylistUrl);
@@ -28,27 +32,35 @@ function App() {
     setId(id);
   };
 
+  const handleInfoButtonClick = () => {
+    navigate("/aboutus");
+  };
+
   return (
     <div className="app-container">
       <div className="header-container">
-        <div className="title-container">
+        <div className="titleContainer">
           <img src="/logo.png" alt="logo" className="logo" />
           <h1 className="title">Melodyssey</h1>
+          <IconButton
+            className="info-btn"
+            aria-label="Open about us"
+            onClick={handleInfoButtonClick}
+          >
+            <Icon className="info-icon">info</Icon>
+          </IconButton>
         </div>
-        <IconButton className="info-btn" aria-label="Open about us">
-          <Icon className="info-icon">info</Icon>
-        </IconButton>
-        <p className="instruction">
-          Введіть посилання на плейлист, виконавця, трек або альбом Spotify:
-        </p>
-        <input
-          type="text"
-          placeholder="Enter Spotify URL"
-          value={spotifyUrl}
-          onChange={handleInputChange}
-          className="input"
-        />
       </div>
+      <p className="instruction">
+        Введіть посилання на плейлист, виконавця, трек або альбом Spotify:
+      </p>
+      <input
+        type="text"
+        placeholder="Enter Spotify URL"
+        value={spotifyUrl}
+        onChange={handleInputChange}
+        className="input"
+      />
       {type && id && <Player className="player" type={type} id={id} />}
     </div>
   );
